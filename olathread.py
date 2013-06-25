fe740a558aefbdd61685d5477f095d27b7141bdf
Victoria Tisdale <vhtisdale@gmail.com>
2013-06-24 21:12:13 -0400

    comment changes to be gone over at some point

--- olathread.py
+++ olathread.py
@@ -16,9 +16,7 @@ class OLAThread(threading.Thread):
     self._rdm_api = RDMAPI(self._client, self._pid_store)
  	
   def run(self):
-    '''
-    creates a SelectServer object and runs it
-    '''
+    """ creates a SelectServer object and runs it """
     self._ss = SelectServer()
     self._ss.AddReadDescriptor(self._client.GetSocket(),
                                self._client.SocketReady)
@@ -27,9 +25,7 @@ class OLAThread(threading.Thread):
     print 'OLA thread finished'
  
   def stop(self):
-    '''
-    terminates SelectServer object (created in self.run())
-    '''
+    """ terminates SelectServer object (created in self.run()). """
     if self._ss is None:
       print 'OLAThread.Stop() called before thread was running'
       return
@@ -38,57 +34,45 @@ class OLAThread(threading.Thread):
     self._ss.Terminate()       
                                                                                                                                
   def run_discovery(self, universe, callback):
-    '''
-    Can be called from any thread.
-    Callback takes two arguments(bool, [UID])
-    Callback is run in the OLA thread.
-    '''
-    self._ss.Execute(lambda : self._run_discovery(universe, callback))                                                                                                                            
- 
-  def Execute(self, cb):
-    print 'calling execute'
-    self._ss.Execute(cb)
- 
+    """ runs discovery in specified universe
+    	
+    	Args:
+    	  universe: int, specifies the universe in which to run discovery
+    	  callback: method called upon discovery, takes two arguments(bool, [UID])
+    
+    	Can be called from any thread.
+    	Callback is run in the OLA thread.
+    """
+    self._ss.Execute(lambda : self._run_discovery(universe, callback))
+     
   def rdm_get(self, universe, uid, sub_device, pid, callback, data = ''):
-    '''
-    get rdm message
-    '''
+    """ Executes, in the ola thread, an rdm inquiry. """
     print 'rdm get'
     self._ss.Execute(lambda: self._rdm_get(universe, uid, sub_device, pid, callback, data) )
  
   def rdm_set(self, universe, uid, sub_device, pid, callback, data):
-    '''
-    send rdm message
-    '''
-    print 'rdm get'
+    """ Executes, in the ola thread, the setting of an rdm variable. """
+    print 'rdm set'
     self._ss.Execute(lambda: self._rdm_set(universe, uid, sub_device, pid, callback, data) )
  
   def _run_discovery(self, universe, callback):
-    '''
-    This method is only run in the OLA thread.
-    '''
+    """ This method is only run in the OLA thread. """
     response = self._client.RunRDMDiscovery(universe, True, callback)
     if response == False:
       callback( False, [] )
     
   def _rdm_get(self, universe, uid, sub_device, pid, callback, data):
-    '''
-    This method is only run in the OLA thread.
-    '''
+    """ This method is only run in the OLA thread. """
     print '_rdm_get'
     self._rdm_api.Get(universe, uid, sub_device, self._pid_store.GetPid(pid), lambda r, d, e: self.complete_get(callback, r, d, e), data)
       
   def _rdm_set(self, universe, uid, sub_device, pid, callback, data):
-    '''
-    This method is only run in the OLA thread.
-    '''
-    print '_rdm_get'
-    self._rdm_api.Set(universe, uid, sub_device, self._pid_store.GetPid(pid), lambda r, d, e: self.complete_get(callback, r, d, e), data)
+    """ This method is only run in the OLA thread. """
+    print '_rdm_set'
+    self._rdm_api.Set(universe, uid, sub_device, self._pid_store.GetPid(pid), lambda r, d, e: self.complete_set(callback, r, d, e), data)
       
   def complete_get(self, callback, response, data, unpack_exception):
-    '''
-    
-    '''
+    """ Checks if the get was a success, calls the callback from run_get. """
     print 'RDM get completed'
     # need to do something with unpack_exception here
     if response.WasAcked() == False:
@@ -97,9 +81,7 @@ class OLAThread(threading.Thread):
       callback( True, data)
     
   def complete_set(self, callback, response, data, unpack_exception):
-    '''
-    
-    '''
+    """ Checks if the set was a success, calls the callback from run_set. """
     print 'RDM set completed'
     # need to do something with the unpack_exception here
     if response.WasAcked() == False:
--- simple_ui.py
+++ simple_ui.py
@@ -7,10 +7,14 @@ import Queue
 import olathread
 
 class DisplayApp:
-  
+  """ Creates the GUI for sending and receiving RDM messages through the ola thread. """
   def __init__(self, width, height):
-    '''
-    '''
+    """ initializes the GUI and the ola thread
+    
+    Args:
+      width: the int value of the width of the tkinter window
+      height: the int value of the height of the tkinter window
+    """
     # Initialize the tk root window
     self.root = tk.Tk()
     self.init_dx = width
@@ -41,18 +45,22 @@ class DisplayApp:
     print 'back from sleep'
     
   def build_frames(self):
-    '''
-    '''
+    """ builds the two tkinter frames that are used as parents for the tkinter widgets
+    	that both control and display the RDM messages.
+    """
     self.cntrl_frame = tk.PanedWindow(self.root)
     self.cntrl_frame.pack(side=tk.TOP, padx=1, pady=1, fill=tk.Y)
     self.info_frame = tk.PanedWindow(self.root)
     self.info_frame.pack(side=tk.TOP, padx=1, pady=2, fill=tk.Y)
     
   def build_cntrl(self):
-    '''
-    builds the top bar of the GUI where the options for the information display are
-    located
-    '''
+    """ Builds the top bar of the GUI.
+    
+    Initializes all the general tkinter control widgets, including:
+      dev_label: tk string variable for the currently selected device
+      id_box: 
+      device_menu:
+    """
     tk.Label( self.cntrl_frame, text='Select\nUniverse:').pack(side = tk.LEFT)
     menu = tk.OptionMenu(self.cntrl_frame, self.universe, *self.universe_list, command=self.set_universe)
     menu.pack(side = tk.LEFT)
@@ -70,9 +78,7 @@ class DisplayApp:
     tk.Checkbutton(self.cntrl_frame).pack(side = tk.LEFT)
 
   def upon_discover(self, status, uids):
-    '''
-    callback for client.RunRDMDiscovery
-    '''
+    """ callback for client.RunRDMDiscovery """
     print 'discovered'
     self.device_menu['menu'].delete(0,'end')
     for uid in uids:
@@ -80,31 +86,30 @@ class DisplayApp:
       self.ola_thread.rdm_get(self.universe.get(), uid, 0, 0x0082, lambda b, s, uid = uid: self.add_device(uid, b, s))
     
   def add_device(self, uid, succeeded, data):
-    '''
-    adds device name to self.devicenames
-    '''
+    """ callback for the rdm_get in upon_discover.
+    	populates self.device_menu"""
     if succeeded == True:
       self._uid_dict[uid] = {'device label': data['label']}
       self.device_menu['menu'].add_command( label = '%s (%s)' %(self._uid_dict[uid]['device label'], uid), command = lambda : self.device_selected(uid) )
 
   def get_identify_complete(self, uid, succeeded, value):
-    '''
-    sets the checkbox's state to that of the currently selected device
-    '''
+    """ Callback for rdm_get in device_selected.
+    
+    	Sets the checkbox's state to that of the currently selected device
+    """
     if succeeded:
       self.id_state.set(value['identify_state'])
 
   def set_universe(self, i):
-    '''
-    sets the int var self.universe to the value of i
-    '''
+    """ sets the int var self.universe to the value of i """
     self.universe.set(i)
         
   def device_selected(self, uid):
-    '''
-    this function will be called by self.device_menu and will display the information for
-    a particular device in the botton half of the GUI
-    '''
+    """ called when a new device is chosen from dev_menu.
+    	
+    	Args: 
+    	  uid: the uid of the newly selected device
+    """
     if uid == self.cur_uid:
       return
     print 'uid: %s\ncur_uid: %s\nid_state: %d' % (uid, self.cur_uid, self.id_state.get())
@@ -113,11 +118,14 @@ class DisplayApp:
     self.cur_uid = uid  
     
   def set_identify_complete(self, uid, succeded, value):
-    '''
-    '''
+    """ callback for the rdm_set in identify """
     print 'rdm set complete'
     
   def identify(self):
+  	""" Command is called by id_box.
+  	
+  		sets the value of the device's identify field based on the value of id_box
+  	"""
     if self.cur_uid is not None:
       self.ola_thread.rdm_set(self.universe.get(), self.cur_uid, 0, 0x1000, lambda b, s, uid = self.cur_uid: self.set_identify_complete(uid, b, s), [self.id_state.get()])
     else:
