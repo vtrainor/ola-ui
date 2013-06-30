7607a3ab0716bad604786ee368d95785bbf7db29
Victoria Tisdale <vhtisdale@gmail.com>
2013-06-30 16:45:36 -0400

    simple notebook working, not integrated with ola

--- notebook.py
+++ notebook.py
@@ -5,25 +5,40 @@ class RDMNotebook:
   def __init__(self, master, width=600, height=400):
     """ Builds the ttk.Notebook """
     self.root = master
-    self._notebook = ttk.Notebook(self.root, width = self.init_dx,
-                            height = (1/3)*self.init_dy)
+    self.init_dx = width
+    self.init_dy = height
+    self._notebook = ttk.Notebook(self.root, name = 'nb')
+    self.populate_tabs()
+    self._notebook.pack()
 
-  def populate_tabs():
+  def populate_tabs(self):
     """ creates the default frames. """
-    _dmx_tab = tk.Frame(self.root, name = '_dmx_tab')
-    # determine pids that will be on this frame and specify them here.
-    self._notebook.add(self.root, text = 'dmx information')
+    print 'creating default tabs...'
+    self.create_tab('tab 1','hello')
+    self.create_tab('tab 2', 'example text')
 
-  def create_tab(self, tab_name, info_dict, pid_list):
+  def create_tab(self, tab_name, words, tab_label = None):
     """ Creates a tab. 
 
         will want to have all the options allowed by the ttk notebook widget to
         be args for this method
+
+        Args:
+          tab_name: string, cannot begin with a capital letter
+          pid_list: list of strings, 
+          tab_label: string that will be displayed on the tab, default set to 
+            None, and tab_name will be on the tab
     """
-    tab = tk.Frame(self.root, name = tab_name)
-    object_dict = []
-    object_dict = self._create_objects(info_dict)
-    notebook.add(tab,text = tab_name)
+    if tab_label is None:
+        tab_label = tab_name
+    # create each Notebook tab in a Frame
+    tab = tk.Frame(self._notebook, name = tab_name)
+    # here, instead of creating a Label, call self._display_info
+    tk.Label(tab, text = words).pack( side = tk.LEFT)
+    # Button to quit app on right
+    btn = tk.Button(tab, text="quit", command = master.quit)
+    btn.pack(side = tk.RIGHT)
+    self._notebook.add(tab, text = tab_label) # add tab to Notebook
 
   def _create_objects(self, tab):
     """ creates a dictionary of all pids that are assigned to the frame, tab.
@@ -46,7 +61,7 @@ class RDMNotebook:
     keys = object_dict.keys()
     for i in range(len(keys)):
       key = keys[i]
-      if i <= len(object_dict)/2
+      if i <= len(object_dict)/2:
         tk.Label(frame, text = key).grid(col = 0, row = i)
         object_dict[key][0].grid(col = 1, row = i)
         object_dict[key][1].grid(col = 2, row = i)
@@ -60,5 +75,12 @@ class RDMNotebook:
     self.root.mainloop()
 
 if __name__ == '__main__':
-  nb=Notebook()
+  root = tk.Tk() # create a top-level window
+
+  master = tk.Frame(root, name='master', width = 200, heigh = 200)
+  master.pack(fill=tk.BOTH) # fill both sides of the parent
+
+  root.title('EZ') # title for top-level window
+
+  nb = RDMNotebook(master)
   nb.main()
