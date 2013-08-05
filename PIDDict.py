e374fa87a82c36dbfe8d37405406c270ba25deca
Victoria Tisdale <vhtisdale@gmail.com>
2013-08-05 10:29:09 -0400

    PIDDict added

---
+++ PIDDict.py
@@ -0,0 +1,21 @@
+LAMP_ON_MODE = {
+  0:"Off",
+  1:"DMX",
+  2:"On",
+  3:"On After Calibration"
+}
+
+LAMP_STATE = {
+  0:"Off",
+  1:"On",
+  2:"Strike",
+  3:"Standby",
+  4:"Not Present",
+  127:"Error"
+}
+
+DISPLAY_INVERT = {
+  0:"Off",
+  1:"On",
+  2:"Auto"
+}
