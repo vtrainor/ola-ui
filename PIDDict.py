7c3af54e7d161f57cc71b847e60edf4ec1923ec7
Victoria Tisdale <vhtisdale@gmail.com>
2013-08-17 16:13:54 -0400

    PIDDict edited

--- PIDDict.py
+++ PIDDict.py
@@ -1,39 +1,31 @@
 LAMP_ON_MODE = {
-  0:"Off",
-  1:"DMX",
-  2:"On",
-  3:"On After Calibration"
+  0: "Off",
+  1: "DMX",
+  2: "On",
+  3: "On After Calibration",
 }
 
 LAMP_STATE = {
-  0:"Off",
-  1:"On",
-  2:"Strike",
-  3:"Standby",
-  4:"Not Present",
-  127:"Error"
+  0: "Off",
+  1: "On",
+  2: "Strike",
+  3: "Standby",
+  4: "Not Present",
+  127: "Error",
 }
 
 DISPLAY_INVERT = {
-  0:"Off",
-  1:"On",
-  2:"Auto"
-}
-
-SENSOR_VALUE = {
-	0: "Recorded Value Supported",
-	1: "Lowest/Highest Detected Values Supported",
-	2: "Reserved",
-	3: "Reserved",
-	4: "Reserved",
-	5: "Reserved",
-	6: "Reserved",
-	7: "Reserved"
+  0: "Off",
+  1: "On",
+  2: "Auto",
 }
 
 POWER_STATE = {
   0: "Full Off",
   1: "Shutdown",
   2: "Standby",
-  255: "Normal"
+  255: "Normal",
 }
+
+RECORDED_SUPPORTED = 0x01
+LOWEST_HIGHEST_SUPPORTED = 0X02
