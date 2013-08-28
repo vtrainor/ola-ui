7628a9de0cf218b9e8c51af7146cd0d80bf920df
Victoria Tisdale <vhtisdale@gmail.com>
2013-08-28 14:45:09 -0400

    change to dialog text value

--- rdm_dialog.py
+++ rdm_dialog.py
@@ -2,13 +2,11 @@ import Tkinter as tk
 
 class RDMDialog:
 
-  def __init__ (self, parent, pid, value):
+  def __init__ (self, parent, error):
     print 'dialog init'
     self.top = tk.Toplevel(parent)
-    self.pid = pid
-    self.value = value
     tk.Label(self.top, text='RDM Dialog').pack()
-    tk.Label(self.top, text='invalid value (%s) for %s' % (value, pid)).pack()
+    tk.Label(self.top, text=error).pack()
     tk.Button(self.top, text='Ok', command=self.ok).pack()
 
 
